5dfaae664dc94a2a1b000491133ebf6011aff751
Robert Karlsson <BobKarlsson@gmail.com>
2013-01-09 08:21:27 -0800

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SweetApp::Application.config.secret_token = '35f0a1b4c1180bdfc10fbe59bb3ab7677eeddd456fad1539f4f8429dfa9f62c2f9da6e34a7444e13b89d037c3943d3aa7d77b1683c4187aa6b225eb22521959f'
