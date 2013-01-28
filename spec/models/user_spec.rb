759f698138dbdee6432ac74dc825ed3ac45599c0
Robert Karlsson <BobKarlsson@gmail.com>
2013-01-27 21:04:26 -0800

    finish sign-in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
 
@@ -29,7 +30,7 @@ describe User do
     it { should_not be_valid }
   end
 
-   describe "when name is too long" do
+  describe "when name is too long" do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
@@ -94,6 +95,11 @@ describe User do
     it { should_not == user_for_invalid_password }
     specify { user_for_invalid_password.should be_false }
   end
-end
+  
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
+end
 end
