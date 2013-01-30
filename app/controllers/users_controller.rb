b1d12d8e01a3b439f4ce21ada13f7d265d992ea5
Robert Karlsson <BobKarlsson@gmail.com>
2013-01-30 10:15:33 -0800

    finish user edit/index/update/destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,77 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :index, :update]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
 
 	def create
-    @user = User.new(params[:user])
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+    redirect_to root_path if signed_in?
+      @user = User.new(params[:user])
+      if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+      else
+        render 'new'
+      end    
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if current_user?(@user)
+      flash[:error] = "Don't kill yourself you stupid admin"
+      redirect_to root_url and return
     else
-      render 'new'
+      @user.destroy
+      flash[:success] = "User destroyed."
+      redirect_to users_url
     end
   end
 
   def new
-  	@user = User.new
+    if signed_in?
+      redirect_to root_url
+  	else
+      @user = User.new
+    end
   end
 
 	def show
     @user = User.find(params[:id])
   end
 
-  
+  def edit # must be correct_user      
+  end
 
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def update # correct_user
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      end  
+    end
+
+    def correct_user  
+      @user = User.find(params[:id]) # moved from update
+      redirect_to(root_path) unless current_user?(@user) # see sessions helper
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 
 end
